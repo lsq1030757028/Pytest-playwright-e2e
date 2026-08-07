5babf29c429389f4282954a25c9ee77e67ffada3
lsq1030757028 <lsq1030757028@gmail.com>
2026-08-07 11:53:27 +0800

    feat(m1b): export recovery migration and benchmark APIs

--- src/test_workflow/memory_store/__init__.py
+++ src/test_workflow/memory_store/__init__.py
@@ -1,4 +1,16 @@
+from .benchmark import (
+    RetrievalBenchmarkCase,
+    RetrievalBenchmarkReport,
+    RetrievalBenchmarkRunner,
+)
 from .index import IndexHit, SQLiteDerivedIndex
+from .migration import MigrationReport, SQLiteMigrationController, StoreManifest
+from .recovery import (
+    FailClosedRetrievalGateway,
+    OutboxHealthReport,
+    OutboxRecoveryReport,
+    SQLiteOutboxRecovery,
+)
 from .resilience import (
     IndexHealthReport,
     IndexHealthStatus,
@@ -25,14 +37,21 @@ from .sqlite import SQLiteMemoryStore
 __all__ = [
     "BudgetConsumption",
     "ChannelContribution",
+    "FailClosedRetrievalGateway",
     "IndexHealthReport",
     "IndexHealthStatus",
     "IndexHit",
     "IndexRebuildReport",
+    "MigrationReport",
+    "OutboxHealthReport",
+    "OutboxRecoveryReport",
     "ProgressiveMemoryRetriever",
     "RecallChannel",
     "ReleasedMemory",
     "ReplayVerification",
+    "RetrievalBenchmarkCase",
+    "RetrievalBenchmarkReport",
+    "RetrievalBenchmarkRunner",
     "RetrievalReplayEvidence",
     "RetrievalReplayVerifier",
     "RetrievalRequest",
@@ -42,5 +61,8 @@ __all__ = [
     "SQLiteDerivedIndex",
     "SQLiteIndexResilience",
     "SQLiteMemoryStore",
+    "SQLiteMigrationController",
+    "SQLiteOutboxRecovery",
     "StageBudget",
+    "StoreManifest",
 ]
