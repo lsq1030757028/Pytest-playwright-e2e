662075499bb22a2402ff3104e2adc74b2312ec84
lsq1030757028 <lsq1030757028@gmail.com>
2026-08-07 11:25:01 +0800

    feat(m1b): expose progressive retrieval runtime [HUMAN:memory-first-i2]

--- src/test_workflow/memory_store/__init__.py
+++ src/test_workflow/memory_store/__init__.py
@@ -1,3 +1,30 @@
+from .index import IndexHit, SQLiteDerivedIndex
+from .retrieval import (
+    BudgetConsumption,
+    ChannelContribution,
+    ProgressiveMemoryRetriever,
+    RecallChannel,
+    ReleasedMemory,
+    RetrievalRequest,
+    RetrievalResult,
+    RetrievalStage,
+    RetrievalStatus,
+    StageBudget,
+)
 from .sqlite import SQLiteMemoryStore
 
-__all__ = ["SQLiteMemoryStore"]
+__all__ = [
+    "BudgetConsumption",
+    "ChannelContribution",
+    "IndexHit",
+    "ProgressiveMemoryRetriever",
+    "RecallChannel",
+    "ReleasedMemory",
+    "RetrievalRequest",
+    "RetrievalResult",
+    "RetrievalStage",
+    "RetrievalStatus",
+    "SQLiteDerivedIndex",
+    "SQLiteMemoryStore",
+    "StageBudget",
+]
